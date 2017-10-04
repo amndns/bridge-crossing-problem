97dbc8010793d01daf83762f5ac54b3cbbc02bd8
Pupperoni <32456314+Pupperoni@users.noreply.github.com>
2017-10-05 00:56:24 +0800

    Add files via upload
    has a more correct heuristic + outputs correct input and is pretty fast

--- astar.py
+++ astar.py
@@ -3,12 +3,12 @@ import bisect, itertools
 class Node:
     def __init__(self, cost, heuristic, state, steps):
         self.cost = cost
-        self.f = cost + heuristic
+        self.evaluation_function = cost + heuristic
         self.state = state
         self.steps = steps
 
     def __lt__(self, other):
-        return self.f < other.f
+        return self.evaluation_function < other.evaluation_function
 
     def print_steps(self):
         i = 0
@@ -32,67 +32,77 @@ class Tree:
 
     def has_repeated_states(self, node):
         for item in self.fringe:
-            if node.state == item.state and node.f < item.f:
+            if node.state == item.state and node.evaluation_function < item.evaluation_function:
                 self.fringe.remove(item)
                 bisect.insort(self.fringe, node)
                 return True
             if node.state == item.state:
                 return True
         return False
-
-    
-    def find_heuristic(self,node,place):
-    	l = []
-    	for elem in list(range(len(self.fitness))):
-    		if node.state[elem] == place:
-    			l.append(self.fitness[elem])
-    	l.sort()
-    	print(l)
-    	if len(l) >= 4:
-    		h = (len(l)-3)*l[0]
-    		for elem in range(1,len(l)-2):
-    			h += l[elem]
-    		h += min(2*l[1],l[0]+l[len(l)-2]) + l[len(l)-1]
-    	elif len(l) == 3:
-    		h = sum(l)
-    	else:
-    		h = max(l)
-    	print(h)
-    	return h
+    '''
+    h = the sum of every other time taken by people on the left of the bridge
+    We choose this heuristic to minimize the wasted time by letting the slowest
+    and 2nd slowest, 3rd slowest and 4th slowest, and so on cross the bridge
+    to "mask" the 2nd person's time
+    '''
+    def find_heuristic(self,node):
+        l = []
+        h = 0
+        for elem in list(range(len(self.fitness))):
+                if node.state[elem] == 0:
+                        l.append(self.fitness[elem])
+        l = l[::-1]
+        for elem in range(len(l),2):
+                h += l[elem]
+        #print(h)
+        return h
 
     def generate_nodes(self,node):
-    	fitness_len = len(self.fitness)
-    	if len(node.steps) % 3 == 0:
-    		h = self.find_heuristic(node,0)
-    		for elem in itertools.combinations(list(range(fitness_len)), 2):
-    			if (node.state[elem[0]] == 0 and node.state[elem[1]] == 0):
-    				new_state = list(node.state)
-    				new_state[elem[0]] = 1
-    				new_state[elem[1]] = 1
+        fitness_len = len(self.fitness)
+        if len(node.steps) % 3 == 0:
+            #print("old " ,node.state)
+            h = self.find_heuristic(node)
+            fin = []
+            left = []
+            c = 0
+            for elem in list(range(fitness_len)):
+                    if node.state[elem] == 0:
+                            left.append(elem)
+                            c += 1
+            if c == 2:
+                fin = [ [left[0],left[1]] ]
+            else:
+                fin = [ [left[0],left[1]] , [left[0],left[-1]] , [left[-2],left[-1]] ]
+            c = 0
+            #print(fin)
+            for elem in fin:
+                    new_state = list(node.state)
+                    
+                    new_state[elem[0]] = 1
+                    new_state[elem[1]] = 1
+                    new_steps = list(node.steps)
+                    new_steps.extend([elem[0]+1, elem[1]+1])
+                    
+                    new_cost = node.cost + max(self.fitness[elem[0]], self.fitness[elem[1]])
+                    new_node = Node(new_cost, h , new_state, new_steps)
+                    #print("new ",new_state)
+                    if not self.has_repeated_states(new_node):
+                            bisect.insort(self.fringe, new_node)
+        else:
+            for elem in list(range(fitness_len)):
+                if (node.state[elem] == 1):
+                    new_state = list(node.state)
+                    new_state[elem] = 0
 
-    				new_steps = list(node.steps)
-    				new_steps.extend([elem[0]+1, elem[1]+1])
+                    new_steps = list(node.steps)
+                    new_steps.append(elem+1)
 
-    				new_cost = node.cost + max(self.fitness[elem[0]], self.fitness[elem[1]])
-    				new_node = Node(new_cost, h	, new_state, new_steps)
+                    new_cost = node.cost + self.fitness[elem]
 
-    				if not self.has_repeated_states(new_node):
-    					bisect.insort(self.fringe, new_node)
-    	else:
-    		for elem in list(range(fitness_len)):
-    			if (node.state[elem] == 1):
-    				new_state = list(node.state)
-    				new_state[elem] = 0
+                    new_node = Node(new_cost, 0, new_state, new_steps)
 
-    				new_steps = list(node.steps)
-    				new_steps.append(elem+1)
-
-    				new_cost = node.cost + self.fitness[elem]
-
-    				new_node = Node(new_cost, 0, new_state, new_steps)
-
-    				if not self.has_repeated_states(new_node):
-    					bisect.insort(self.fringe, new_node)
+                    if not self.has_repeated_states(new_node):
+                        bisect.insort(self.fringe, new_node)
 
     def a_star(self):
         num_visited = 0;
@@ -100,11 +110,10 @@ class Tree:
         while True:
             if not self.fringe:
                 return 'Failure'
-
+            
             # Explore Fringe
             node = self.fringe.pop(0)
             num_visited += 1
-
             if node.state == [1] * fitness_len:
                 print(node.cost, num_visited)
                 node.print_steps()
@@ -112,13 +121,10 @@ class Tree:
 
             self.generate_nodes(node)
 
-inp = [6,8,14,33,59]
+inp = [1,2,5,10,12,17,24,21,20,20,11,33,15,19,55]
 #[1,2,5,9,15,15,19]
+inp.sort()
 state_space = Tree(inp)
 state_space.a_star()
 
 
-'''
-
-
-'''
